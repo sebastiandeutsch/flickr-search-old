0b152070910572f2b31df01eb5271082b1ed2ab5
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2016-09-07 10:13:23 +0200

    Initial import

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './containers/App';
+import 'stylesheets/reset.css';
+import 'stylesheets/application.css';
+
+React.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ reducers/index.js
@@ -0,0 +1,50 @@
+import Immutable from 'immutable';
+import * as Actions from 'constants/ActionTypes';
+
+let history = [];
+
+const defaultPhotoState = Immutable.fromJS({
+    searchText: null,
+    page: null,
+    photos: [],
+    status: null
+});
+
+const actionsMap = {
+  [Actions.SEARCH_PENDING]: (state, action) => {
+    return state.merge({
+      status: 'PENDING'
+    });
+  },
+
+  [Actions.SEARCH_RESULT]: (state, action) => {
+    var results = action.payload;
+    var photos = results.photos;
+    if (results.page != 1 && results.searchText === state.get('searchText')) {
+      photos =  state.get('photos').concat(Immutable.fromJS(photos));
+    }
+
+    return state.merge({
+      searchText: results.searchText,
+      page: results.page,
+      photos: photos,
+      status: 'DONE'
+    });
+  },
+
+  [Actions.UNDO]: (state, action) => {
+    ...
+  }
+}
+
+export function photos(state = defaultPhotoState, action) {
+  const reduceFn = actionsMap[ action.type ]
+
+  history.push(state);
+
+  if (!reduceFn) {
+    return state
+  }
+
+  return reduceFn(state, action)
+};
